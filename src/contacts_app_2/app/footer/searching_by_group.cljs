c00eff61a24b31287922c11180530f4be2ade1ff
cl-journeyor <genericobject711@yahoo.com>
2026-04-10 21:52:48 -0700

    Fixed state bug at searching by group; edits at index.html

--- src/contacts_app_2/app/footer/searching_by_group.cljs
+++ src/contacts_app_2/app/footer/searching_by_group.cljs
@@ -4,58 +4,59 @@
             [phosphor.icons :as icons]
             [reagent.core :as r]))
 
-(def ^:private search-text (r/atom ""))
-
-(defn- cancel!
-  []
-  (swap!
-   sh/state
-   (fn [prev]
-     {:selected-contact nil
-      :status (sh/statuses :reading)
-      :widget-states (mapv
-                      #(assoc % :visible? true)
-                      (prev :widget-states))})))
-
-(defn- handle-input-change!
-  [e]
-  (reset! search-text (-> e .-target .-value)))
-
-(defn- search!
-  [e]
-  (.preventDefault e)
-  (let [search-txt
-        (.toLocaleLowerCase @search-text)
-
-        visible-widget-mapper
-        (fn [widget-state]
-          (assoc
-           widget-state
-           :visible?
-           (some
-            #(str/includes? (.toLocaleLowerCase %) search-txt)
-            (-> widget-state :contact :groups))))]
-    (swap!
-     sh/state
-     (fn [prev]
-       (update prev :widget-states #(mapv visible-widget-mapper %))))))
-
 (defn searching-by-group
   []
-  [:<>
-   [:button.secondary-iconic-btn {:type "button"
-                                  :on-click cancel!}
-    (-> (icons/icon :phosphor.regular/arrow-left)
-        (icons/render (sh/icon-widths :large)))]
-   [:form.footer-field-group {:id "search-by-group-form"
-                              :on-submit search!}
-    [:label.footer-label {:for "search-by-group-field"}
-     "Search by group"]
-    [:input.text-field {:id "search-by-group-field"
-                        :name "search-text"
-                        :type "text"
-                        :on-change handle-input-change!
-                        :auto-focus true}]]
-   [:button.primary-iconic-btn {:form "search-by-group-form"}
-    (-> (icons/icon :phosphor.regular/magnifying-glass)
-        (icons/render (sh/icon-widths :large)))]])
+  (let [search-text
+        (r/atom "")
+
+        cancel!
+        (fn []
+          (swap!
+           sh/state
+           (fn [prev]
+             {:selected-contact nil
+              :status (sh/statuses :reading)
+              :widget-states (mapv
+                              #(assoc % :visible? true)
+                              (prev :widget-states))})))
+
+        handle-input-change!
+        (fn [e]
+          (reset! search-text (-> e .-target .-value)))
+
+        search!
+        (fn [e]
+          (.preventDefault e)
+          (let [search-txt
+                (.toLocaleLowerCase @search-text)
+
+                visible-widget-mapper
+                (fn [widget-state]
+                  (assoc
+                   widget-state
+                   :visible?
+                   (some
+                    #(str/includes? (.toLocaleLowerCase %) search-txt)
+                    (-> widget-state :contact :groups))))]
+            (swap!
+             sh/state
+             (fn [prev]
+               (update prev :widget-states #(mapv visible-widget-mapper %))))))]
+    (fn []
+      [:<>
+       [:button.secondary-iconic-btn {:type "button"
+                                      :on-click cancel!}
+        (-> (icons/icon :phosphor.regular/arrow-left)
+            (icons/render (sh/icon-widths :large)))]
+       [:form.footer-field-group {:id "search-by-group-form"
+                                  :on-submit search!}
+        [:label.footer-label {:for "search-by-group-field"}
+         "Search by group"]
+        [:input.text-field {:id "search-by-group-field"
+                            :name "search-text"
+                            :type "text"
+                            :on-change handle-input-change!
+                            :auto-focus true}]]
+       [:button.primary-iconic-btn {:form "search-by-group-form"}
+        (-> (icons/icon :phosphor.regular/magnifying-glass)
+            (icons/render (sh/icon-widths :large)))]])))
